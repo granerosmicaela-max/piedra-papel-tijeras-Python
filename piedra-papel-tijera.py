7e0ccbc3c15fa085248d61e8b158f77b0c76cb99
granerosmicaela-max <granerosmicaela@gmail.com>
2026-06-02 20:07:15 -0300

    Improve player input prompts and case sensitivity

--- piedra-papel-tijera.py
+++ piedra-papel-tijera.py
@@ -1,12 +1,12 @@
 nombre1 = input("¿Cómo se llamará el jugador 1: " )
 nombre2 = input("¿Cómo se llamará el jugador 2: ")
 
-jugador1 = input("Hola ¿Qué eliges? ¿Piedra,papel o tijera?: ")
-jugador2 = input("Hola jugador 2 ¿Qué eliges? ¿Piedra,papel o tijera?: ")
+jugador1 = input(nombre1 + " ¿Qué eliges? ¿Piedra,papel o tijera?: ")
+jugador2 = input(nombre2+" ¿Qué eliges? ¿Piedra,papel o tijera?: ")
 
-if(jugador1==jugador2):
+if(jugador1.lower()==jugador2.lower()):
     print("¡Ha sido un empate!")
-elif(jugador1== "Piedra"and jugador2== "tijera")or(jugador1== "papel" and jugador2 == "Piedra") or(jugador1== "Tijera" and jugador2== "papel"):
+elif(jugador1.lower()== "Piedra"and jugador2.lower()== "tijera")or(jugador1.lower()== "papel" and jugador2.lower() == "Piedra") or(jugador1.lower()== "Tijera" and jugador2.lower()== "papel"):
     print ("Ha ganado:",nombre1)
 else:
     print("Ha ganado:",nombre2)
